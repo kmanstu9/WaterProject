dcefad5a68c00cc1e359b2b69c5ef3cefec32099
kmanstu9 <kmanstu9@byu.edu>
2025-03-18 13:20:41 -0600

    Initial project commit

---
+++ backend/WaterProject.API/Controllers/WaterController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WaterProject.API.Data;
+
+namespace Mission10Stuart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WaterController : ControllerBase
+    {
+        private WaterDbContext _context;
+
+        public WaterController(WaterDbContext temp)
+        {
+            _context = temp;
+        }
+
+        [HttpGet(Name = "GetProject")]
+        public IEnumerable<Project> GetProjects()
+        {
+            var ProjectList = _context.Projects.ToList();
+
+            return (ProjectList);
+        }
+    }
+}
